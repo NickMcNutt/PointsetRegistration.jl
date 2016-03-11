d7cd69fbbf3d859c4ff6aa1aa45d8aab81a32998
Nick McNutt <nmcnutt@utk.edu>
2016-03-11 05:10:52 -0500

    Changed everything

--- src/PointsetRegistration.jl
+++ src/PointsetRegistration.jl
@@ -1,17 +1,41 @@
 module PointsetRegistration
 
-using MoreMatrices
-using Munkres
-
 export
-    # exact_pairwise.jl
-    register,
+    # accelerated_routines.jl
+    neg_XtRY, wYPXt, cost, partial_cost,
 
-    # permutation_synchronization.jl
-    synchronize_permutations
+    # clustering.jl
+    kernel_kmeans, max_k_cut, factor_k_cut,
 
-include("exact_pairwise.jl")
-include("exact_pairwise_multi.jl")
-include("permutation_synchronization.jl")
+    # grid.jl
+    grid, grid_coords,
+
+    # GPM.jl
+    GPM_orthogonal, GPM_permutation,
+
+    # density_grid.jl
+    parallel_density_grid, density_grid, density_grid_stats,
+
+    # mayavi_plotting.jl
+    figure, plot_points, axes, sync,
+
+    # register.jl
+    register, iterative_register, register_exact,
+
+    # synchronization.jl
+    maximize_trace, parallel_pairwise_register, project_to_stiefel, stiefel_to_permutations,
+
+    # pointsets.jl
+    trim, random_perturb!, random_permute!, random_rotate!, pointsets_transform, pointsets_error
+
+include("accelerated_routines.jl")
+include("clustering.jl")
+include("grid.jl")
+include("GPM.jl")
+include("density_grid.jl")
+include("mayavi_plotting.jl")
+include("register.jl")
+include("synchronization.jl")
+include("pointsets.jl")
 
 end
