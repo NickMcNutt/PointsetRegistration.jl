2aa4f3f066d2f98a3cf59bbb8d4fee345cab3ecf
Nick McNutt <nmcnutt@utk.edu>
2016-03-16 20:33:16 -0400

    Added Gaussian registration and registration to a reference

--- src/register.jl
+++ src/register.jl
@@ -1,5 +1,6 @@
 import Munkres: munkres
 import Combinatorics: permutations
+import MoreMatrices: rotation!
 
 function register{N, T}(weights::NTuple{N, T}, X::NTuple{N, Matrix{T}}, Y::NTuple{N, Matrix{T}}, C::Matrix{T}, invert::Bool = false)
     M = Matrix{T}[Matrix{T}(size(X[i], 2), size(X[i], 2)) for i in 1:N]
@@ -22,7 +23,7 @@ function register{N, T}(weights::NTuple{N, T}, X::NTuple{N, Matrix{T}}, Y::NTupl
         
         x^2 + y^2 + z^2 > π^2 && continue
             
-        MoreMatrices.rotation!(R, x, y, z)
+        rotation!(R, x, y, z)
         
         if invert
             R[3, 1], R[3, 2], R[3, 3] = -R[3, 1], -R[3, 2], -R[3, 3]
@@ -61,97 +62,7 @@ function register{N, T}(weights::NTuple{N, T}, X::NTuple{N, Matrix{T}}, Y::NTupl
     return sqrt(abs(cost_min)), Q_min, P_min
 end
 
-function iterative_register{N, T}(w::NTuple{N, T}, S::Vector{Matrix{T}}, A::Vector{Matrix{T}}, G::Matrix{T}, invert::Bool = false)
-    s_max = 0
-    R_max = Matrix{T}(3, 3)
-    p_max = Vector{Vector{Int}}(N)
-    
-    R = eye(T, 3)
-    p = Vector{Vector{Int}}(N)
-    Q = Matrix{T}(3, 3)
-    
-    m = size(G, 2)
-    for i in 1:m
-        x, y, z = G[1, i], G[2, i], G[3, i]
-        
-        x^2 + y^2 + z^2 > π^2 && continue
-            
-        MoreMatrices.rotation!(R, x, y, z)
-        
-        if invert
-            R[3, 1], R[3, 2], R[3, 3] = -R[3, 1], -R[3, 2], -R[3, 3]
-        end   
-        
-        fill!(Q, 0)
-        
-        for j in 1:N
-            p[j] = munkres(-(S[j] * R * A[j])')
-            Q += w[j] * A[j][:, p[j]] * S[j]
-        end
-        
-        s = sum(svdvals(Q))
-        
-        if s > s_max
-            s_max = s
-            copy!(R_max, nearest_orthogonal(Q'))
-            
-            for j in 1:N
-                p_max[j] = p[j]
-            end
-        end
-    end
-    
-    return s_max, R_max, p_max
-end
-
-function iterative_register{N, T}(weights::NTuple{N, T}, X::NTuple{N, Vector{Matrix{T}}})
-    m = length(X[1])
-    G = grid_coords(1π, 16)
-    A = Vector{Matrix{T}}[deepcopy(Y) for Y in X]
-    B = Vector{Matrix{T}}(N)
-    S = Vector{Matrix{T}}(N)
-    
-    @inbounds for t in 1:8
-        for k in 1:m
-            s_max = 0
-            R_max = Matrix{T}(3, 3)
-            p_max = Vector{Vector{Int}}(N)
-
-            for j in 1:N
-                S[j] = sum(A[j])' - A[j][k]'
-                #S[j] -= A[j][k]'
-                B[j] = A[j][k]
-            end     
-            
-            s_max, R_max, p_max = iterative_register(weights, S, B, G, false)
-            s_max2, R_max2, p_max2 = iterative_register(weights, S, B, G, true)
-            
-            if s_max2 > s_max
-                s_max = s_max2
-                R_max = R_max2
-                p_max = p_max2
-            end
-            
-            for j in 1:N
-                A[j][k] = R_max * B[j][:, p_max[j]]
-            end
-            
-            s = 0
-            for j in 1:N
-                s += weights[j] * vecnorm(mean(A[j]))^2
-            end
-                
-            print("\r$t\t$k: $s")
-            flush(STDOUT)
-        end
-        
-        println()
-    end
-    
-    return map(mean, A), A
-end
-
-function register_exact{T}(X::Matrix{T}, Y::Matrix{T})
+function exact_register{T}(X::Matrix{T}, Y::Matrix{T})
     d, n = size(X)
     s_max = -Inf
     p_min = Vector{Int}(n)
@@ -166,4 +77,69 @@ function register_exact{T}(X::Matrix{T}, Y::Matrix{T})
     P_min = eye(n)[:, p_min]
 
     return vecnorm(X - R_min * Y * P_min), R_min, P_min
+end
+
+function gaussian_register{T}(σ::T, X::Matrix{T}, Y::Matrix{T}, G::Matrix{T})
+    num_samples = size(G, 2)
+    n = size(X, 2)
+    R = Matrix{T}(3, 3)
+    R_min = Matrix{T}(3, 3)
+    
+    s_max = 0
+    for k in 1:num_samples
+        x, y, z = G[1, k], G[2, k], G[3, k]
+        
+        x^2 + y^2 + z^2 > π^2 && continue
+        
+        rotation!(R, x, y, z)
+        
+        s = 0
+        for i in 1:n, j in 1:n
+            s += exp(vecnorm(X[:, i] - R * Y[:, j])^2 / (-2σ^2))
+        end
+        
+        if s > s_max
+            s_max = s
+            copy!(R_min, R)
+        end
+    end
+    
+    return R_min
+end
+
+function register_to_reference{N, T}(g::Int, reference::NTuple{N, Matrix{T}}, kinds::NTuple{N, Vector{Matrix{T}}}, weights::NTuple{N, T})
+    n = length(kinds[1])
+    queue = []
+    
+    for i in 1:n
+        X = ntuple(N) do (k) reference[k] end
+        Y = ntuple(N) do (k) kinds[k][i] end
+        push!(queue, (weights, X, Y))
+    end
+        
+    cells = pmap(queue) do pair
+        weights, X, Y = pair
+        G = grid_coords(1π, g)   
+        d1, R1, P1 = register(weights, X, Y, G, false)
+        d2, R2, P2 = register(weights, X, Y, G, true)
+        if d1 < d2
+            return d1, R1, P1
+        else
+            return d2, R2, P2
+        end
+    end
+    
+    R = Vector{Matrix{T}}(n)
+    P = ntuple(N) do (k) Vector{Matrix{T}}(n) end
+    
+    for i in 1:n
+        d, r, p = shift!(cells)
+        R[i] = r
+        
+        for k in 1:N
+            P[k][i] = p[k]'
+        end
+    end
+    
+    return R, P 
 end
